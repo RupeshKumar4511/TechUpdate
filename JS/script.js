1033b2cb0067959773a90f571c50ed09151a87ed
RupeshKumar4511 <kumarrn4511@gmail.com>
2024-09-24 18:35:54 +0530

    Project Updated

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+let currentIndex = 0;
+let forward = true;  // Track the direction of the slide (true means forward, false means backward)
+const slides = document.querySelectorAll('.slider img');
+const totalSlides = slides.length;
+const slider = document.querySelector('.slider');  // Target the slider container
+const slideInterval = setInterval(changeSlide, 3000); // Change slide every 3 seconds
+
+function changeSlide() {
+    if (forward) {
+        currentIndex++;
+        if (currentIndex >= totalSlides - 1) {
+            forward = false;  // Change direction when the last slide is reached
+        }
+    } else {
+        currentIndex--;
+        if (currentIndex <= 0) {
+            forward = true;  // Change direction when the first slide is reached
+        }
+    }
+
+    // Translate the slider horizontally based on the current index
+    slider.style.transform = `translateX(-${currentIndex * 100}vw)`;
+    slider.style.transition = 'transform 0.8s ease';  // Smooth transition effect
+}
+
+
+
+let message = document.querySelector("#contact");
+let dialog = document.querySelector(".popup")
+message.addEventListener("click",()=>{
+dialog.show()
+});
